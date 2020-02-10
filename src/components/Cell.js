18d71ab9fa280499356a033759bd7e10ba29401f
Jesse Martinez <jezze.04@gmail.com>
2020-02-10 14:42:07 -0600

    downloaded bean images. added bean onclick to card

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,12 +1,32 @@
 import React, {PureComponent} from 'react';
 
+import Bean from '../assets/beans/bean1.png';
 
 class Cell extends PureComponent {
+
+  state={
+    beanPlaced: false,
+  }
+
+  handleClick = (event) => {
+    this.setState({beanPlaced: !this.state.beanPlaced})
+  }
+
+
   render(){
     const {id} = this.props;
+    const {beanPlaced} = this.state;
     return(
-      <div className="cell-container">
+      <div className="cell-container" onClick={this.handleClick}>
         <img className="cell-img" src={id.src} alt={id.name} />
+        <img src={Bean} alt='bean'
+          style={{
+            position: 'absolute',
+            height: '50px',
+            marginLeft: '15px',
+            marginTop: '-100px',
+            display: (beanPlaced)?'block':'none'
+          }}/>
       </div>
     );
   }
